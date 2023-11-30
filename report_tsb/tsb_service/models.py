28f1f4b75fbbad79b973a5fa7b906e6f7a98ba28
jenriordan <jennifer.riordan@digital.trade.gov.uk>
2023-11-30 09:47:20 +0000

    update notes, json

--- report_tsb/tsb_service/models.py
+++ report_tsb/tsb_service/models.py
@@ -7,6 +7,7 @@ from django.db import models
 class BreachDetails(models.Model):
     # might not be needed, as django can auto create a primary key
     report_id = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
+    # most likely redundant as the json will hold date data
     report_date = models.DateField(auto_now=True)
     # TODO: add auto json validation
     data = models.JSONField(default=dict)
