5ed7777c13a52b7769382cae5db2bf9f91721312
jenriordan <jennifer.riordan@digital.trade.gov.uk>
2023-12-01 09:26:55 +0000

    address pr comments and update req file

--- report_tsb/tsb_service/models.py
+++ report_tsb/tsb_service/models.py
@@ -6,7 +6,8 @@ from django.db import models
 # Create your models here.
 class BreachDetails(models.Model):
     # might not be needed, as django can auto create a primary key
-    report_id = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
+    # TODO: run makemigrations
+    report_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     # most likely redundant as the json will hold date data
     report_date = models.DateField(auto_now=True)
     # TODO: add auto json validation
