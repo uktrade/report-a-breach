eded37edc6a71025360ea2d64285e40c75e45a83
jenriordan <jennifer.riordan@digital.trade.gov.uk>
2023-12-11 11:23:34 +0000

    update export breadcrumb link

--- report_trade_sanctions_breach/report_breach_web_service/constants.py
+++ report_trade_sanctions_breach/report_breach_web_service/constants.py
@@ -4,6 +4,6 @@ BREADCRUMBS_START_PAGE = [
     ("Home", "https://www.gov.uk"),
     ("Business and Industry", "https://www.gov.uk/business-and-industry"),
     ("Trade and Investment", "https://www.gov.uk/business-and-industry/trade-and-investment"),
-    ("Exporting", "https://www.gov.uk/check-duties-customs-exporting"),
+    ("Exporting", "https://www.gov.uk/business-and-industry/exporting"),
     ("Embargoes and sanctions", "https://www.gov.uk/business-and-industry/embargoes-and-sanctions"),
 ]
