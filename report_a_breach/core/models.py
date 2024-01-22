a3e4c3a2fc10dbfbac138d4a4294479687b5eaef
christopherpettinga <chrispettinga@gmail.com>
2024-01-22 14:24:55 +0000

    added baseModel and poetry.lock

--- report_a_breach/core/models.py
+++ report_a_breach/core/models.py
@@ -2,11 +2,11 @@ import uuid
 
 from django.db import models
 
+from report_a_breach.base_classes.models import BaseModel
+
 
 # This model is a WIP while we are awaiting the final schema
-class BreachDetails(models.Model):
-    # might not be needed, as django can auto create a primary key
-    report_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+class BreachDetails(BaseModel):
     reporter_full_name = models.CharField(null=False)
     reporter_email_address = models.EmailField(null=False)
     reporter_professional_relationship = models.TextField(null=False)
