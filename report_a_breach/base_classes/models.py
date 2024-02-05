32de2c3ad11977afa797bd31206a4c893548ca44
Nawaz <nawaz.ahmed@digital.trade.gov.uk>
2024-02-05 09:12:12 +0000

    add history inheritance flag

--- report_a_breach/base_classes/models.py
+++ report_a_breach/base_classes/models.py
@@ -8,7 +8,7 @@ class BaseModel(models.Model):
     id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
-    history = HistoricalRecords()
+    history = HistoricalRecords(inherit=True)
 
     class Meta:
         abstract = True
