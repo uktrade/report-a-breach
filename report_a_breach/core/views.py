c69d6a3c591c6b85a88cfdf5d28c11837a55f29d
christopherpettinga <chrispettinga@gmail.com>
2024-02-06 10:23:21 +0000

    fixing process_email_step

--- report_a_breach/core/views.py
+++ report_a_breach/core/views.py
@@ -37,7 +37,7 @@ class ReportABreachWizardView(BaseWizardView):
     def get_summary_context_data(self, form):
         return self.get_all_cleaned_data()
 
-    def process_summary_step(self, form):
+    def process_email_step(self, form):
         reporter_email_address = form.cleaned_data.get("reporter_email_address")
         verify_code = get_random_string(6, allowed_chars="0123456789")
         self.request.session["verify_code"] = verify_code
