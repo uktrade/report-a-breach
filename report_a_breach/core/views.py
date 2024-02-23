e215c99bd2f96439bb50ad9694f59c1850eb4a5b
jenriordan <jennifer.riordan@digital.trade.gov.uk>
2024-02-23 15:53:14 +0000

    fix sanctions regime commit

--- report_a_breach/core/views.py
+++ report_a_breach/core/views.py
@@ -95,13 +95,15 @@ class ReportABreachWizardView(BaseWizardView):
             what_were_the_goods=all_cleaned_data["what_were_the_goods"],
         )
 
-        if declared_sanction := all_cleaned_data["which_sanctions_regime"]:
-            sanctions_regime = SanctionsRegime.objects.get(full_name=declared_sanction)
-            sanctions_breach = SanctionsRegimeBreachThrough.objects.create(
-                breach=new_breach, sanctions_regime=sanctions_regime
-            )
-            sanctions_breach.save()
-            new_breach.sanctions_regimes.add(sanctions_regime)
+        if declared_sanctions := all_cleaned_data["which_sanctions_regime"]:
+            print(declared_sanctions)
+            for sanction in declared_sanctions:
+                sanctions_regime = SanctionsRegime.objects.get(full_name=sanction)
+                sanctions_breach = SanctionsRegimeBreachThrough.objects.create(
+                    breach=new_breach, sanctions_regime=sanctions_regime
+                )
+                sanctions_breach.save()
+                new_breach.sanctions_regimes.add(sanctions_regime)
 
         # temporary, to be removed when the forms are integrated into the user journey
         new_breach.additional_information = "N/A"
