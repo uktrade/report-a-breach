5bd0e220b152734703645dde0f812eb3f0f7a930
christopherpettinga <chrispettinga@gmail.com>
2024-03-05 14:52:12 +0000

    Adding ECS django log formatter
    Added the ECSFormatter as a production logging formatter, this allows logs to be formatted in accordance with the ELK demands and appends zipkin headers to the logs

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,3 +1,5 @@
+from django_log_formatter_ecs import ECSFormatter
+
 from .base import *  # noqa
 
 DEBUG = False
@@ -5,3 +7,21 @@ DEBUG = False
 INSTALLED_APPS += ["django_audit_log_middleware"]
 
 MIDDLEWARE += ["django_audit_log_middleware.AuditLogMiddleware"]
+
+LOGGING = {
+    "formatters": {
+        "ecs_formatter": {
+            "()": ECSFormatter,
+        },
+    },
+    "handlers": {
+        "ecs": {
+            "formatter": "ecs_formatter",
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["ecs"],
+        },
+    },
+}
