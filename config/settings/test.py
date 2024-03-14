89c3619e7f0f5aeaff2d9781cb366b08c7b00877
Morgan Rees <morganrees@DBT000491.local>
2024-03-14 17:03:03 +0000

    Initial playwright breach workflow

--- config/settings/test.py
+++ config/settings/test.py
@@ -1,1 +1,13 @@
+import environ
+
 from .base import *  # noqa
+
+env = environ.Env(
+    DEBUG=(bool, False),
+)
+
+TEST_EMAIL_VERIFY_CODE = True
+
+HEADLESS = env.bool("HEADLESS", default=True)
+
+BASE_FRONTEND_TESTING_URL = env.str("BASE_FRONTEND_TESTING_URL", default="http://localhost:8000")
