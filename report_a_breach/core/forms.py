b4d09ada10ceb36215c8d9a40445674c3d702e9e
Morgan Rees <65167892+morganmaerees@users.noreply.github.com>
2024-03-19 17:14:10 +0000

    Remove forms.py whitespace

--- report_a_breach/core/forms.py
+++ report_a_breach/core/forms.py
@@ -197,7 +197,7 @@ class DoYouKnowTheRegisteredCompanyNumberForm(BaseModelForm):
                 cleaned_data["registered_office_address"] = get_formatted_address(company_details["registered_office_address"])
             except CompaniesHouseException:
                 self.add_error("registered_company_number", "The company number you entered is not valid")
-                
+
         return cleaned_data
 
 
