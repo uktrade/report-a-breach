f8a12cc3e66b717b139862d609a08114a7ff3fdb
Morgan Rees <morganrees@DBT000491.local>
2024-03-26 16:36:11 +0000

    DST-335/download-files

--- report_a_breach/urls.py
+++ report_a_breach/urls.py
@@ -1,6 +1,8 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import include, path
 
 urlpatterns = [
     path("", include("report_a_breach.core.urls")),
     path("healthcheck", include("healthcheck.urls")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
