32dcd6c4110e3ec4adcd8e6cac4af0c4a01bf3e6
christopherpettinga <chrispettinga@gmail.com>
2024-04-10 16:28:50 +0100

    adding debugging statements

--- django_app/core/views.py
+++ django_app/core/views.py
@@ -129,12 +129,14 @@ class RedirectBaseDomainView(RedirectView):
 
     def get(self, request, *args, **kwargs):
         url = self.get_redirect_url(*args, **kwargs)
-        site = request.site
+        current_site_name = request.site.name
+        current_site_domain = request.site.domain
         host = request.get_host()
         raw_host = request._get_raw_host()
         site_domains = [each.domain for each in Site.objects.all()]
         site_name = [each.name for each in Site.objects.all()]
-        print(site)
+        print(current_site_name)
+        print(current_site_domain)
         print(host)
         print(site_domains)
         print(site_name)
