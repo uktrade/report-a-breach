be3f712f7ddc3ffd2d14841225ae59622b6eab8f
christopherpettinga <chrispettinga@gmail.com>
2024-04-10 09:31:51 +0100

    Added sys path appending to wsgi.py

--- django_app/config/wsgi.py
+++ django_app/config/wsgi.py
@@ -8,10 +8,13 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
+from pathlib import Path
 
 from django.core.wsgi import get_wsgi_application
 from opentelemetry.instrumentation.wsgi import OpenTelemetryMiddleware
 
+sys.path.append(Path(__file__).resolve().parent.parent.__str__())
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.prod")
 
 application = get_wsgi_application()
