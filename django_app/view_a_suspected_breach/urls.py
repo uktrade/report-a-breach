716fe821bd3e772e0447e2aa890e0a80ba1bb3f0
Morgan Rees <morganrees@DBT000491.local>
2024-04-15 17:27:48 +0100

    multi file upload

--- django_app/view_a_suspected_breach/urls.py
+++ django_app/view_a_suspected_breach/urls.py
@@ -6,4 +6,5 @@ app_name = "view_a_suspected_breach"
 
 urlpatterns = [
     path("", views.ViewABreachView.as_view(), name="landing"),
+    path("files", views.FileFieldFormView.as_view(), name="files"),
 ]
--- django_app/view_a_suspected_breach/views.py
+++ django_app/view_a_suspected_breach/views.py
@@ -1,10 +1,26 @@
 from core.sites import require_view_a_breach
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
+from django.views.generic.edit import FormView
+
 from django.views.generic import TemplateView
+
+from .forms import FileFieldForm
 
 
 @method_decorator(login_required, name="dispatch")
 @method_decorator(require_view_a_breach(), name="dispatch")
 class ViewABreachView(TemplateView):
     template_name = "view_a_suspected_breach/landing.html"
+
+
+class FileFieldFormView(FormView):
+    form_class = FileFieldForm
+    template_name = "view_a_suspected_breach/upload_documents.html"  # Replace with your template.
+    success_url = "..."  # Replace with your URL or reverse().
+
+    def form_valid(self, form):
+        files = form.cleaned_data["file_field"]
+        for f in files:
+            print(f)
+        return super().form_valid(form)
