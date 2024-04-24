0df2dd88261952ab1760933ea7bb9b400c2ea9c8
jenriordan <150944262+jenriordan@users.noreply.github.com>
2024-04-24 14:58:35 +0100

    Update django_app/core/context_processors.py
    Co-authored-by: Christopher Pettinga <chrispettinga@gmail.com>

--- django_app/core/context_processors.py
+++ django_app/core/context_processors.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.http import HttpRequest
 
 
-def truncate_words_limit(request: HttpRequest) -> dict[str, Any]:
+def truncate_words_limit(request: HttpRequest) -> dict[str, int]:
     return {
         "truncate_words_limit": settings.TRUNCATE_WORDS_LIMIT,
     }
