257059e819a9d0506bcf9f6905430cf12337e807
jenriordan <jennifer.riordan@digital.trade.gov.uk>
2024-05-07 10:49:00 +0100

    initial commit

--- django_app/view_a_suspected_breach/models.py
+++ django_app/view_a_suspected_breach/models.py
@@ -1,1 +1,11 @@
-# Create your models here.
+from core.models import BaseModel
+from django.db import models
+
+
+class User(BaseModel):
+    email = models.EmailField(unique=True)
+    is_pending = models.BooleanField(default=True)
+
+
+class AdminUser(BaseModel):
+    email = models.EmailField(unique=True)
