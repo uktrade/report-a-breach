99199621fcd8c893f33f271b5c413333de4b308c
Kudrotey <kudrotey.khuda@digital.trade.gov.uk>
2024-05-08 15:57:05 +0100

    updates image_build_run.sh

--- .copilot/image_build_run.sh
+++ .copilot/image_build_run.sh
@@ -4,10 +4,10 @@
 set -e
 
 # Add commands below to run inside the container after all the other buildpacks have been applied
+export COPILOT_ENVIRONMENT_NAME='build'
 export DJANGO_SECRET_KEY='dummy_key'
 export DEBUG='True'
 export DATABASE_URL='psql://postgres:password@127.0.0.1:15432/postgres'
-export DJANGO_SETTINGS_MODULE='config.settings.local'
 
 echo "Running django_app/manage.py collectstatic --noinput"
 python django_app/manage.py collectstatic --no-input
