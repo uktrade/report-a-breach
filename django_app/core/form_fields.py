c7b732eb13a21b41296d45e1d2a810acf0f64355
jenriordan <jennifer.riordan@digital.trade.gov.uk>
2024-05-08 08:51:58 +0100

    extra ws

--- django_app/core/form_fields.py
+++ django_app/core/form_fields.py
@@ -2,7 +2,6 @@ from django import forms
 
 
 class BooleanChoiceField(forms.ChoiceField):
-
     def to_python(self, value: str | bool) -> bool:
         if isinstance(value, str) and value.lower() in ("false", "0"):
             value = False
