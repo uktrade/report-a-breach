3757b6270dea03ae1d6f5ee227ab2bdaadd06504
Kudrotey <kudrotey.khuda@digital.trade.gov.uk>
2024-05-09 11:39:40 +0100

    Update env var to use database_credentials instead of database_url.

--- .copilot/image_build_run.sh
+++ .copilot/image_build_run.sh
@@ -4,10 +4,11 @@
 set -e
 
 # Add commands below to run inside the container after all the other buildpacks have been applied
+export BUILD_STEP='True'
 export COPILOT_ENVIRONMENT_NAME='build'
 export DJANGO_SECRET_KEY='dummy_key'
 export DEBUG='True'
-export DATABASE_URL='psql://postgres:password@127.0.0.1:15432/postgres'
+export DATABASE_CREDENTIALS='{"username": "postgres", "password": "password", "engine": "postgres", "port": 5432, "dbname": "postgres", "host": "db", "dbInstanceIdentifier": "emt-db"}'
 export DJANGO_SETTINGS_MODULE='config.settings.deploy.development'
 
 echo "Running django_app/manage.py collectstatic --noinput"
--- .copilot/phases/build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Exit early if something goes wrong
-set -e
-
-# Add commands below to run as part of the build phase
-export BUILD_STEP='True'
