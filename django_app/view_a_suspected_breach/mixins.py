403fe4823b8fcf6b32145d4c88c3f82820752358
jenriordan <jennifer.riordan@digital.trade.gov.uk>
2024-05-15 09:53:17 +0100

    update email context

--- django_app/view_a_suspected_breach/mixins.py
+++ django_app/view_a_suspected_breach/mixins.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.conf import settings
 from django.contrib.auth.models import User
 from django.http import HttpRequest, HttpResponse
@@ -6,14 +8,20 @@ from utils.notifier import send_email
 
 
 class ActiveUserRequiredMixin:
-    def dispatch(self, request: HttpRequest, *args: object, **kwargs: object) -> HttpResponse:
+    def dispatch(self, request: HttpRequest, **kwargs: object) -> HttpResponse:
         if not request.user.is_active:
+            admin_url = reverse("view_a_suspected_breach:user_admin")
+            user_login_datetime = f"{datetime.now():%Y-%m-%d %H:%M:%S%z}"
             for user in User.objects.filter(is_staff=True):
                 if user.is_staff:
                     send_email(
                         email=user.email,
                         template_id=settings.EMAIL_VASB_USER_ADMIN_TEMPLATE_ID,
-                        context={"admin_url": reverse("view_a_suspected_breach:user_admin")},
+                        context={
+                            "user_email": request.user.email,
+                            "user_login_datetime": user_login_datetime,
+                            "admin_url": f"{settings.VIEW_A_SUSPECTED_BREACH_DOMAIN}{admin_url}",
+                        },
                     )
             return render(request, "view_a_suspected_breach/unauthorised.html")
 
