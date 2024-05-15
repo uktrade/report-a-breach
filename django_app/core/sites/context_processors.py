77c1149b0064bcfa8d30c0ad2c3a9b24413d1112
christopherpettinga <chrispettinga@gmail.com>
2024-05-15 16:05:00 +0100

    Fixing issue with sites context processor as was causing with dbt platform

--- django_app/core/sites/context_processors.py
+++ django_app/core/sites/context_processors.py
@@ -3,8 +3,9 @@ from django.http import HttpRequest
 from . import is_report_a_suspected_breach_site, is_view_a_suspected_breach_site
 
 
-def sites(request: HttpRequest) -> dict[str, bool]:
-    return {
-        "is_report_a_suspected_breach_site": is_report_a_suspected_breach_site(request.site),
-        "is_view_a_suspected_breach_site": is_view_a_suspected_breach_site(request.site),
-    }
+def sites(request: HttpRequest) -> dict[str, bool] | None:
+    if hasattr(request, "site"):
+        return {
+            "is_report_a_suspected_breach_site": is_report_a_suspected_breach_site(request.site),
+            "is_view_a_suspected_breach_site": is_view_a_suspected_breach_site(request.site),
+        }
