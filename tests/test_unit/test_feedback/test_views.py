9c4626da1bcfb149085fe197f5a6b77b5adfef9a
christopherpettinga <chrispettinga@gmail.com>
2024-06-03 14:23:00 +0100

    fixing tests

--- tests/test_unit/test_feedback/test_views.py
+++ tests/test_unit/test_feedback/test_views.py
@@ -1,21 +1,31 @@
 from django.urls import reverse
 from feedback.models import FeedbackItem
+from freezegun import freeze_time
 
 
 class TestProvideFullFeedbackView:
+
+    @freeze_time("2012-01-14")
     def test_successful(self, rasb_client):
         assert FeedbackItem.objects.count() == 0
         response = rasb_client.post(
             reverse("feedback:collect_full_feedback"),
             data={
                 "rating": 1,
-                "what_did_not_work_so_well": ["process_not_clear"],
                 "how_we_could_improve_the_service": "try harder",
+                "did_you_experience_any_issues": ["not_found", "lacks_features"],
             },
         )
         assert FeedbackItem.objects.count() == 1
         assert response.status_code == 302
         assert response.url == reverse("feedback:feedback_done")
+        feedback_object = FeedbackItem.objects.get()
+        assert feedback_object.rating == 1
+        assert feedback_object.how_we_could_improve_the_service == "try harder"
+        assert feedback_object.did_you_experience_any_issues == ["not_found", "lacks_features"]
+        assert feedback_object.created_at.year == 2012
+        assert feedback_object.created_at.month == 1
+        assert feedback_object.created_at.day == 14
 
     def test_unsuccessful(self, rasb_client):
         response = rasb_client.post(
@@ -37,6 +47,7 @@ class TestProvideFullFeedbackView:
             data={
                 "rating": 1,
                 "how_we_could_improve_the_service": "try harder",
+                "did_you_experience_any_issues": ["not_found", "lacks_features"],
             },
         )
         feedback.refresh_from_db()
