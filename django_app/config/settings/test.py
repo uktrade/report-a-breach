98040b798dff06da1f11cebb15b1663c651d3991
jenriordan <jennifer.riordan@digital.trade.gov.uk>
2024-06-04 09:14:50 +0100

    update test

--- django_app/config/settings/test.py
+++ django_app/config/settings/test.py
@@ -14,10 +14,7 @@ BASE_FRONTEND_TESTING_URL = "http://report-a-suspected-breach:8000"
 ENVIRONMENT = "test"
 
 # we don't want to connect to ClamAV in testing, redefine and remove from list
-FILE_UPLOAD_HANDLERS = (
-    "core.custom_upload_handler.CustomFileUploadHandler",
-    "django.core.files.uploadhandler.TemporaryFileUploadHandler",
-)  # Order is important
+FILE_UPLOAD_HANDLERS = ("django_chunk_upload_handlers.s3.S3FileUploadHandler",)  # Order is important
 
 
 # don't use redis when testing
