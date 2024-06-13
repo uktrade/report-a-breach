9a6c6a8815e64a28fa19874a9aa15ce0a902014d
Morgan Rees <morganrees@DBT000491.local>
2024-06-13 15:08:43 +0100

    testing your details

---
+++ tests/test_frontend/test_email.py
@@ -0,0 +1,30 @@
+import re
+
+from playwright.sync_api import expect
+
+from . import conftest
+
+
+class TestVerify(conftest.PlaywrightTestBase):
+    def test_correct_email_goes_to_verify_page(self):
+        self.page.goto(self.get_form_step_page("email"))
+        self.page.get_by_label("What is your email address?").click()
+        self.page.get_by_label("What is your email address?").fill("test@gmail.com")
+        self.page.get_by_role("button", name="Continue").click()
+        expect(self.page).to_have_url(re.compile(r".*/email_verify"))
+
+    def test_no_email_raises_error(self):
+        self.page.goto(self.get_form_step_page("email"))
+        self.page.get_by_role("button", name="Continue").click()
+        self.page.get_by_label("There is a problem").click()
+        self.page.get_by_role("link", name="Enter your email address").click()
+        expect(self.page).to_have_url(re.compile(r".*/email"))
+
+    def test_incorrect_format_raises_error(self):
+        self.page.goto(self.get_form_step_page("email"))
+        self.page.get_by_label("What is your email address?").click()
+        self.page.get_by_label("What is your email address?").fill("testemail")
+        self.page.get_by_role("button", name="Continue").click()
+        self.page.get_by_label("There is a problem").click()
+        self.page.get_by_role("link", name="Enter an email in the correct format").click()
+        expect(self.page).to_have_url(re.compile(r".*/email"))
---
+++ tests/test_frontend/test_your_details.py
@@ -0,0 +1,70 @@
+import re
+
+from playwright.sync_api import expect
+
+from . import conftest
+
+
+class TestNameAndBusinessYouWorkFor(conftest.PlaywrightTestBase):
+    def test_correct_details_goes_to_taslkist_page(self):
+        self.page.goto("http://report-a-suspected-breach:8000/report_a_suspected_breach/")
+        self.page.get_by_role("link", name="Your details").click()
+        self.reporter_professional_relationship(self.page, "I work for a third party")
+        self.verify_email_details(self.page)
+        self.page.get_by_role("heading", name="Your details").click()
+        self.page.get_by_label("Full name").click()
+        self.page.get_by_label("Full name").fill("John Smith")
+        self.page.get_by_label("Business you work for").click()
+        self.page.get_by_label("Business you work for").fill("Business")
+        self.page.get_by_role("button", name="Continue").click()
+        expect(self.page).to_have_url(re.compile(r".*/are_you_reporting_a_business_on_companies_house"))
+
+    def test_no_name_raises_error(self):
+        self.page.goto("http://report-a-suspected-breach:8000/report_a_suspected_breach/")
+        self.page.get_by_role("link", name="Your details").click()
+        self.reporter_professional_relationship(self.page, "I work for a third party")
+        self.verify_email_details(self.page)
+        self.page.get_by_role("heading", name="Your details").click()
+        self.page.get_by_label("Business you work for").click()
+        self.page.get_by_label("Business you work for").fill("Business")
+        self.page.get_by_role("button", name="Continue").click()
+        self.page.get_by_label("There is a problem").click()
+        self.page.get_by_role("link", name="Enter your full name").click()
+        expect(self.page).to_have_url(re.compile(r".*/name_and_business_you_work_for"))
+
+    def test_no_business_raises_error(self):
+        self.page.goto("http://report-a-suspected-breach:8000/report_a_suspected_breach/")
+        self.page.get_by_role("link", name="Your details").click()
+        self.reporter_professional_relationship(self.page, "I work for a third party")
+        self.verify_email_details(self.page)
+        self.page.get_by_role("heading", name="Your details").click()
+        self.page.get_by_label("Full name").click()
+        self.page.get_by_label("Full name").fill("John Smith")
+        self.page.get_by_role("button", name="Continue").click()
+        self.page.get_by_label("There is a problem").click()
+        self.page.get_by_role("link", name="Enter the name of the business you work for").click()
+        expect(self.page).to_have_url(re.compile(r".*/name_and_business_you_work_for"))
+
+
+class TestName(conftest.PlaywrightTestBase):
+    def test_correct_details_goes_to_taslkist_page(self):
+        self.page.goto("http://report-a-suspected-breach:8000/report_a_suspected_breach/")
+        self.page.get_by_role("link", name="Your details").click()
+        self.reporter_professional_relationship(self.page, "I'm an owner, officer or")
+        self.verify_email_details(self.page)
+        self.page.get_by_text("What is your full name?", exact=True).click()
+        self.page.get_by_label("What is your full name?").click()
+        self.page.get_by_label("What is your full name?").fill("John Smith")
+        self.page.get_by_role("button", name="Continue").click()
+        expect(self.page).to_have_url(re.compile(r".*/are_you_reporting_a_business_on_companies_house"))
+
+    def test_no_name_raises_error(self):
+        self.page.goto("http://report-a-suspected-breach:8000/report_a_suspected_breach/")
+        self.page.get_by_role("link", name="Your details").click()
+        self.reporter_professional_relationship(self.page, "I'm an owner, officer or")
+        self.verify_email_details(self.page)
+        self.page.get_by_text("What is your full name?", exact=True).click()
+        self.page.get_by_role("button", name="Continue").click()
+        self.page.get_by_label("There is a problem").click()
+        self.page.get_by_role("link", name="Enter your full name").click()
+        expect(self.page).to_have_url(re.compile(r".*/name"))
