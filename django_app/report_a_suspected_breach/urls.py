071ecc1cd08836a43bf46a3b927833f9206298ba
jenriordan <jennifer.riordan@digital.trade.gov.uk>
2024-07-15 15:55:56 +0100

    views for about the business

--- django_app/report_a_suspected_breach/urls.py
+++ django_app/report_a_suspected_breach/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from report_a_suspected_breach.views import generic, views_a
+from report_a_suspected_breach.views import generic, views_a, views_b
 
 app_name = "report_a_suspected_breach"
 
@@ -16,6 +16,24 @@ views_a_urls = [
         "name_and_business_you_work_for", views_a.NameAndBusinessYouWorkForView.as_view(), name="name_and_business_you_work_for"
     ),
     path("name", views_a.YourNameView.as_view(), name="name"),
+    path(
+        "are_you_reporting_a_business_on_companies_house",
+        views_b.AreYouReportingCompaniesHouseBusinessView.as_view(),
+        name="are_you_reporting_a_business_on_companies_house",
+    ),
+    path(
+        "do_you_know_the_registered_company_number",
+        views_b.DoYouKnowTheRegisteredCompanyNumberView.as_view(),
+        name="do_you_know_the_registered_company_number",
+    ),
+    path("manual_companies_house_input", views_b.ManualCompaniesHouseView.as_view(), name="manual_companies_house_input"),
+    path("check_company_details", views_b.CheckCompanyDetailsView.as_view(), name="check_company_details"),
+    path(
+        "where_is_the_address_of_the_business_or_person",
+        views_b.WhereIsTheAddressOfTheBusinessOrPersonView.as_view(),
+        name="where_is_the_address_of_the_business_or_person",
+    ),
+    path("business_or_person_details", views_b.BusinessOrPersonDetailsView.as_view(), name="business_or_person_details"),
 ]
 
 urlpatterns = generic_urls + views_a_urls
