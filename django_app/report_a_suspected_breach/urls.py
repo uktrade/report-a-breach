c7aaffa0d3e28557e03268380f2dfd5ddd591c62
Morgan Rees <morganrees@DBT000491.local>
2024-07-16 18:08:53 +0100

    Sanctions breach details views

--- django_app/report_a_suspected_breach/urls.py
+++ django_app/report_a_suspected_breach/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from report_a_suspected_breach.views import generic, views_a
+from report_a_suspected_breach.views import generic, views_a, views_e
 
 app_name = "report_a_suspected_breach"
 
@@ -18,7 +18,18 @@ views_a_urls = [
     path("name", views_a.YourNameView.as_view(), name="name"),
 ]
 
-urlpatterns = generic_urls + views_a_urls
+views_e_urls = [
+    path("upload_documents", views_e.UploadDocumentsView.as_view(), name="upload_documents"),
+    path("delete_documents", views_e.DeleteDocumentsView.as_view(), name="delete_documents"),
+    path("download_document/<str:file_name>", views_e.DownloadDocumentView.as_view(), name="download_document"),
+    path(
+        "tell_us_about_the_suspected_breach",
+        views_e.TellUsAboutTheSuspectedBreachView.as_view(),
+        name="tell_us_about_the_suspected_breach",
+    ),
+]
+
+urlpatterns = generic_urls + views_a_urls + views_e_urls
 
 step_to_view_dict = {}
 view_to_step_dict = {}
