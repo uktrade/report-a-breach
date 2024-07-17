ce285f7c04c3904d611586309d5ed8fda3787341
jenriordan <jennifer.riordan@digital.trade.gov.uk>
2024-07-17 11:47:53 +0100

    remove views b

--- django_app/report_a_suspected_breach/urls.py
+++ django_app/report_a_suspected_breach/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from report_a_suspected_breach.views import generic, views_a, views_b, views_c
+from report_a_suspected_breach.views import generic, views_a, views_c
 
 app_name = "report_a_suspected_breach"
 
@@ -18,30 +18,7 @@ views_a_urls = [
     path("name", views_a.YourNameView.as_view(), name="name"),
 ]
 
-view_b_urls = [
-    path(
-        "are_you_reporting_a_business_on_companies_house",
-        views_b.AreYouReportingCompaniesHouseBusinessView.as_view(),
-        name="are_you_reporting_a_business_on_companies_house",
-    ),
-    path(
-        "do_you_know_the_registered_company_number",
-        views_b.DoYouKnowTheRegisteredCompanyNumberView.as_view(),
-        name="do_you_know_the_registered_company_number",
-    ),
-    path("manual_companies_house_input", views_b.ManualCompaniesHouseView.as_view(), name="manual_companies_house_input"),
-    path("check_company_details", views_b.CheckCompanyDetailsView.as_view(), name="check_company_details"),
-    path(
-        "where_is_the_address_of_the_business_or_person",
-        views_b.WhereIsTheAddressOfTheBusinessOrPersonView.as_view(),
-        name="where_is_the_address_of_the_business_or_person",
-    ),
-    path(
-        "business_or_person_details/<str:is_uk_address>/",
-        views_b.BusinessOrPersonDetailsView.as_view(),
-        name="business_or_person_details",
-    ),
-]
+view_b_urls = []
 
 view_c_urls = [
     path("when_did_you_first_suspect", views_c.WhenDidYouFirstSuspectView.as_view(), name="when_did_you_first_suspect"),
