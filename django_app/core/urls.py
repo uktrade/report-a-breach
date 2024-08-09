c5a40d7274838c173138d1834e8d344bfec4382a
jenriordan <jennifer.riordan@digital.trade.gov.uk>
2024-08-09 07:13:23 +0100

    comment out redirect view

--- django_app/core/urls.py
+++ django_app/core/urls.py
@@ -1,15 +1,14 @@
-from core.views import (
+from core.views import (  # RedirectBaseDomainView,
     CookiesConsentView,
     HealthCheckView,
     HideCookiesView,
-    RedirectBaseDomainView,
     ResetSessionView,
 )
 from django.conf import settings
 from django.urls import include, path
 
 urlpatterns = [
-    path("", RedirectBaseDomainView.as_view(), name="initial_redirect_view"),
+    # path("", RedirectBaseDomainView.as_view(), name="initial_redirect_view"),
     path("report_a_suspected_breach/", include("report_a_suspected_breach.urls")),
     path("view_a_suspected_breach/", include("view_a_suspected_breach.urls")),
     path("cookies_consent", CookiesConsentView.as_view(), name="cookies_consent"),
