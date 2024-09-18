670292fba4739741f8210618baf40aab74f30de0
Morgan Rees <65167892+morganmaerees@users.noreply.github.com>
2024-09-18 09:56:33 +0100

    DST-623: Upload documents fix for file extensions (#200)
    Error messaging for file types
    Allow all casing for file extensions
    
    ---------
    
    Co-authored-by: Morgan Rees <morganrees@DBT000491.local>

--- django_app/report_a_suspected_breach/forms/forms_e.py
+++ django_app/report_a_suspected_breach/forms/forms_e.py
@@ -6,7 +6,6 @@ from core.utils import get_mime_type
 from crispy_forms_gds.helper import FormHelper
 from crispy_forms_gds.layout import Field, Layout
 from django import forms
-from django.utils.html import escape
 from django_chunk_upload_handlers.clam_av import VirusFoundInFileException
 from report_a_suspected_breach.fields import MultipleFileField
 from report_a_suspected_breach.models import Breach, UploadedDocument
@@ -41,7 +40,7 @@ class UploadDocumentsForm(BaseForm):
             except VirusFoundInFileException:
                 documents.remove(document)
                 raise forms.ValidationError(
-                    "A virus was found in one of the files you uploaded.",
+                    "The selected file contains a virus",
                 )
 
             # is the document a valid file type?
@@ -69,9 +68,7 @@ class UploadDocumentsForm(BaseForm):
                 "image/jpeg",
                 "image/png",
             ]
-
-            _, file_extension = os.path.splitext(document.name)
-            valid_extension = file_extension in [
+            valid_extensions = [
                 # word documents
                 ".doc",
                 ".docx",
@@ -100,10 +97,14 @@ class UploadDocumentsForm(BaseForm):
                 ".png",
             ]
 
+            _, file_extension = os.path.splitext(document.name)
+            valid_extension = file_extension.lower() in valid_extensions
+            valid_file_type = (
+                ", ".join(valid_extensions[:-1]).replace(".", "").upper() + " or " + valid_extensions[-1].replace(".", "").upper()
+            )
+
             if not valid_mimetype or not valid_extension:
-                raise forms.ValidationError(
-                    f"{escape(document.name)} cannot be uploaded, it is not a valid file type", code="invalid_file_type"
-                )
+                raise forms.ValidationError(f"The selected file must be a {valid_file_type}", code="invalid_file_type")
 
             # has the user already uploaded 10 files?
             if session_files := get_all_session_files(TemporaryDocumentStorage(), self.request.session):
@@ -112,7 +113,7 @@ class UploadDocumentsForm(BaseForm):
 
             # is the document too large?
             if document.size > 104857600:
-                raise forms.ValidationError(f"{document.name} must be smaller than 100 MB", code="too_large")
+                raise forms.ValidationError("The selected file must be smaller than 100 MB", code="too_large")
 
         return documents
 
