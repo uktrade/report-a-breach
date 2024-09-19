6a1253fc9f80648fdd330df0cd9ef04022d22940
Morgan Rees <65167892+morganmaerees@users.noreply.github.com>
2024-09-19 15:46:38 +0100

    DST-622: Submit button text (#203)
    Co-authored-by: Morgan Rees <morganrees@DBT000491.local>

--- django_app/report_a_suspected_breach/forms/forms_f.py
+++ django_app/report_a_suspected_breach/forms/forms_f.py
@@ -3,6 +3,7 @@ from django import forms
 
 
 class DeclarationForm(BaseForm):
+    submit_button_text = "Submit"
     declaration = forms.BooleanField(
         label="I agree and accept", required=True, error_messages={"required": "Confirm if you agree and accept the declaration"}
     )
