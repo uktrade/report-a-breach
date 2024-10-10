0ea59bc641c0d8b238087364b42f449e97d7483d
Morgan Rees <morganrees@DBT000491.local>
2024-10-10 15:46:19 +0100

    DST-759: enable sentry tracing

--- django_app/config/settings/base.py
+++ django_app/config/settings/base.py
@@ -213,7 +213,8 @@ if SENTRY_DSN and SENTRY_ENVIRONMENT:
         dsn=SENTRY_DSN,
         environment=SENTRY_ENVIRONMENT,
         integrations=[DjangoIntegration()],
-        traces_sample_rate=0,
+        enable_tracing=env.bool("SENTRY_ENABLE_TRACING", False),
+        traces_sample_rate=env.float("SENTRY_TRACES_SAMPLE_RATE", 0.0),
     )
 
 # Email Verification settings
