65cabe3475884c9fa44e57985315eb94a8b21127
Morgan Rees <morganrees@DBT000491.local>
2024-11-12 10:36:46 +0000

    feedback view

--- django_app/feedback/models.py
+++ django_app/feedback/models.py
@@ -12,3 +12,10 @@ class FeedbackItem(BaseModel):
     )
     how_we_could_improve_the_service = models.TextField(null=True, blank=True)
     url = models.URLField(null=True, blank=True)
+
+    def get_did_you_experience_any_issues_display(self) -> list[str]:
+        display = []
+        if self.did_you_experience_any_issues:
+            display += [dict(DidYouExperienceAnyIssues.choices)[value] for value in self.did_you_experience_any_issues]
+        display = ",\n".join(display)
+        return display
