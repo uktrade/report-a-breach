4bd36fc1c275615456bd6c632dc2fa730f6fcd0a
Christopher Pettinga <chris.pettinga@digital.trade.gov.uk>
2024-11-22 19:27:42 +0000

    missing commits

---
+++ django_app/utils/address_formatter.py
@@ -0,0 +1,50 @@
+"""Collection of functions to standardise and format address data"""
+
+from typing import Any, Dict, TypedDict
+
+from django_countries import countries
+
+
+class AddressData(TypedDict):
+    address_line_1: str
+    address_line_2: str
+    town_or_city: str
+    postal_code: str
+    country: str  # ISO 3166-1 alpha-2 code
+
+
+# we place this outside the function as it's fairly costly and won't change
+country_name_to_code = {name: code for code, name in countries}
+
+
+def turn_companies_house_into_normal_address_dict(address_dict: Dict[str, Any]) -> AddressData:
+    new_address_dict = address_dict.copy()
+    # companies house returns a whole country name, we need to convert it to a country code
+    new_address_dict["country"] = country_name_to_code[address_dict["country"]]
+    # companies house uses locality for town_or_city
+    new_address_dict["town_or_city"] = address_dict["locality"]
+
+    return new_address_dict
+
+
+def get_formatted_address(address_dict: AddressData) -> str:
+    """Get formatted, human-readable address from an AddressData address dict"""
+    address_string = ""
+
+    if line_1 := address_dict.get("address_line_1"):
+        address_string += line_1
+
+    if line_2 := address_dict.get("address_line_2"):
+        address_string += f",\n {line_2}"
+
+    if town_or_city := address_dict.get("town_or_city"):
+        address_string += f",\n {town_or_city}"
+
+    if postal_code := address_dict.get("postal_code"):
+        address_string += f",\n {postal_code}"
+
+    if country_code := address_dict.get("country"):
+        country = countries.name(country_code)
+        address_string += f",\n {country}"
+
+    return address_string
