47a8d74355ab1b49c10406aa5e4b4f9990904983
jenriordan <jennifer.riordan@digital.trade.gov.uk>
2024-12-06 15:26:33 +0000

    revert logging

--- django_app/config/settings/test.py
+++ django_app/config/settings/test.py
@@ -63,10 +63,27 @@ CACHES = {"default": {"BACKEND": "config.settings.test.TestingCache"}}
 
 def test_process_email_step(self, form: Form) -> dict[str, Any]:
     """Monkey-patching the process_email_step of the journey to always use the same verify code for testing."""
+
+    from django.contrib.sessions.models import Session
+    from report_a_suspected_breach.models import ReporterEmailVerification
+
+    verify_code = "012345"
+    user_session = Session.objects.get(session_key=self.request.session.session_key)
+    ReporterEmailVerification.objects.create(
+        reporter_session=user_session,
+        email_verification_code=verify_code,
+    )
+
+    return self.get_form_step_data(form)
+
+
+def test_request_verify_code(self, form: Form) -> HttpResponse:
+    """Monkey-patching the form_valid of the request verify code view to always use the same verify code for testing."""
     import logging
 
     from django.contrib.sessions.models import Session
     from report_a_suspected_breach.models import ReporterEmailVerification
+    from report_a_suspected_breach.views.views_start import EmailVerifyView
 
     logger = logging.getLogger(__name__)
 
@@ -77,23 +94,6 @@ def test_process_email_step(self, form: Form) -> dict[str, Any]:
     if getattr(self.request, "limited", False):
         logger.warning(f"User has been rate-limited: {reporter_email_address}")
         return self.form_invalid(form)
-    ReporterEmailVerification.objects.create(
-        reporter_session=user_session,
-        email_verification_code=verify_code,
-    )
-    print(verify_code)
-    return self.get_form_step_data(form)
-
-
-def test_request_verify_code(self, form: Form) -> HttpResponse:
-    """Monkey-patching the form_valid of the request verify code view to always use the same verify code for testing."""
-
-    from django.contrib.sessions.models import Session
-    from report_a_suspected_breach.models import ReporterEmailVerification
-    from report_a_suspected_breach.views.views_start import EmailVerifyView
-
-    verify_code = "012345"
-    user_session = Session.objects.get(session_key=self.request.session.session_key)
     ReporterEmailVerification.objects.create(
         reporter_session=user_session,
         email_verification_code=verify_code,
