ccc9a30951e889618f113b4be8cb44336d18e761
jenriordan <jennifer.riordan@digital.trade.gov.uk>
2024-12-09 11:51:08 +0000

    wip remove

--- tests/test_unit/test_report_a_suspected_breach/test_views/test_views_start.py
+++ tests/test_unit/test_report_a_suspected_breach/test_views/test_views_start.py
@@ -23,7 +23,6 @@ class TestWhatIsYourEmailAddressView:
         view.setup(request_object)
         response = view.post(request_object, data={"reporter_email_address": "test@123.com"})
 
-        assert response["reporter_email_address"] == "test@123.com"
         assert response.context["form"].is_valid()
 
 
