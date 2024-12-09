f98ef345b597d46b71b3c22ad6746a16ca5d9b84
jenriordan <jennifer.riordan@digital.trade.gov.uk>
2024-12-09 11:56:57 +0000

    debug test

--- tests/test_unit/test_report_a_suspected_breach/test_views/test_views_start.py
+++ tests/test_unit/test_report_a_suspected_breach/test_views/test_views_start.py
@@ -5,23 +5,23 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.test import RequestFactory
 from django.urls import reverse
 from report_a_suspected_breach.models import ReporterEmailVerification, Session
-from report_a_suspected_breach.views.views_start import (
+from report_a_suspected_breach.views.views_start import (  # WhatIsYourEmailAddressView,
     EmailVerifyView,
-    WhatIsYourEmailAddressView,
 )
 
 
 class TestWhatIsYourEmailAddressView:
     def test_post(self, rasb_client):
-        request_object = RequestFactory().get("/")
-        request_object.session = rasb_client.session
-        # response = rasb_client.post(
-        #     reverse("report_a_suspected_breach:email"),
-        #     data={"reporter_email_address": "test@123.com"},
-        # )
-        view = WhatIsYourEmailAddressView()
-        view.setup(request_object)
-        response = view.post(request_object, data={"reporter_email_address": "test@123.com"})
+        # request_object = RequestFactory().get("/")
+        # request_object.session = rasb_client.session
+        response = rasb_client.post(
+            reverse("report_a_suspected_breach:email"),
+            data={"reporter_email_address": "test@123.com"},
+            follow=True,
+        )
+        # view = WhatIsYourEmailAddressView()
+        # view.setup(request_object)
+        # response = view.post(request_object, data={"reporter_email_address": "test@123.com"})
 
         assert response.context["form"].is_valid()
 
