4b315a3c52650c79c1649f9db2b4e983ccd8e0f7
jenriordan <jennifer.riordan@digital.trade.gov.uk>
2024-12-30 14:31:56 +0000

    initial commit

--- django_app/healthcheck/checks/s3.py
+++ django_app/healthcheck/checks/s3.py
@@ -1,18 +1,22 @@
+import boto3
 import sentry_sdk
-from core.document_storage import PermanentDocumentStorage, TemporaryDocumentStorage
+
+from django_app.config import settings
 
 
 def s3_check() -> bool:
     """
     Performs a check on the S3 connection
     """
-    temporary_document_bucket = TemporaryDocumentStorage().bucket
-    permanent_document_bucket = PermanentDocumentStorage().bucket
+    client = boto3.client("s3", region_name=settings.AWS_S3_REGION_NAME)
+
+    bucket_names = [settings.TEMPORARY_S3_BUCKET_NAME, settings.PERMANENT_S3_BUCKET_NAME]
 
     try:
-        assert temporary_document_bucket.creation_date
-        assert permanent_document_bucket.creation_date
-        return True
+        for bucket_name in bucket_names:
+            client.head_bucket(Bucket=bucket_name)
     except Exception as e:
         sentry_sdk.capture_exception(e)
         return False
+
+    return True
