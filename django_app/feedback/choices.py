90cd0899e18c93b4d43e222b5bf04304da2a519e
jenriordan <jennifer.riordan@digital.trade.gov.uk>
2024-12-30 11:27:26 +0000

    update branch

--- django_app/feedback/choices.py
+++ django_app/feedback/choices.py
@@ -1,15 +1,22 @@
+from typing import List
+
 from django.db import models
 
 
 class RatingChoices(models.IntegerChoices):
-    VERY_SATISFIED = 5, "Very satisfied"
-    SATISFIED = 4, "Satisfied"
-    NEUTRAL = 3, "Neither satisfied nor dissatisfied"
-    DISSATISFIED = 2, "Dissatisfied"
     VERY_DISSATISFIED = 1, "Very dissatisfied"
+    DISSATISFIED = 2, "Dissatisfied"
+    NEUTRAL = 3, "Neutral"
+    SATISFIED = 4, "Satisfied"
+    VERY_SATISFIED = 5, "Very satisfied"
 
 
 class DidYouExperienceAnyIssues(models.TextChoices):
+    @classmethod
+    def inactive_choices(cls) -> List[str]:
+        return ["no"]
+
+    NO = "no", "I did not experience any issues"
     NOT_FOUND = "not_found", "I did not find what I was looking for"
     DIFFICULT = "difficult", "I found it difficult to navigate"
     LACKS_FEATURES = "lacks_features", "The system lacks the feature I need"
