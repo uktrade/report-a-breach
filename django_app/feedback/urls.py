c62a559c4024e2252313f821d945e9e46dd2ec76
jenriordan <jennifer.riordan@digital.trade.gov.uk>
2024-12-31 09:44:26 +0000

    fix commit

--- django_app/feedback/urls.py
+++ django_app/feedback/urls.py
@@ -6,5 +6,5 @@ app_name = "feedback"
 
 urlpatterns = [
     path("", views.ProvideFullFeedbackView.as_view(), name="collect_full_feedback"),
-    path("feedback-sent/", views.FeedbackDoneView.as_view(), name="feedback_done"),
+    path("done", views.FeedbackDoneView.as_view(), name="feedback_done"),
 ]
