ae2cca09f270d41aa075d8388a38d71fe4cb5806
jenriordan <150944262+jenriordan@users.noreply.github.com>
2025-01-07 16:54:49 +0000

    DST-889: Refactor the RAB frontend tests (#292)

--- tests/test_frontend/test_report_a_suspected_breach/test_supply_chain/test_where_were_goods_supplied_from.py
+++ tests/test_frontend/test_report_a_suspected_breach/test_supply_chain/test_where_were_goods_supplied_from.py
@@ -2,7 +2,7 @@ import re
 
 from playwright.sync_api import expect
 
-from tests.test_frontend import conftest
+from tests.test_frontend import conftest, url_paths
 
 
 class TestWhereWereTheGoodsSuppliedFrom(conftest.PlaywrightTestBase):
@@ -11,8 +11,6 @@ class TestWhereWereTheGoodsSuppliedFrom(conftest.PlaywrightTestBase):
     """
 
     def test_no_input_returns_error(self):
-        self.page.goto(self.base_url)
-        self.page.get_by_role("link", name="Reset session").click()
         self.page.get_by_role("link", name="Your details").click()
         self.create_reporter_details(self.page, "I'm an owner")
         self.page.get_by_role("link", name="2. About the person or").click()
@@ -29,11 +27,9 @@ class TestWhereWereTheGoodsSuppliedFrom(conftest.PlaywrightTestBase):
                 name="Select where the goods, services, technological assistance or technology were supplied from",
             )
         ).to_be_visible()
-        expect(self.page).to_have_url(re.compile(r".*/location-where-supplied-from"))
+        expect(self.page).to_have_url(re.compile(rf".*/{url_paths.LOCATION_SUPPLIED_FROM}"))
 
     def test_uk_options_returns_uk_address_capture(self):
-        self.page.goto(self.base_url)
-        self.page.get_by_role("link", name="Reset session").click()
         self.page.get_by_role("link", name="Your details").click()
         self.create_reporter_details(self.page, "I'm an owner")
         self.page.get_by_role("link", name="2. About the person or").click()
@@ -44,13 +40,11 @@ class TestWhereWereTheGoodsSuppliedFrom(conftest.PlaywrightTestBase):
         self.page.get_by_role("heading", name="Where were the goods,").click()
         self.page.get_by_label("The UK", exact=True).check()
         self.page.get_by_role("button", name="Continue").click()
-        expect(self.page).to_have_url(re.compile(r".*/supplier-details"))
+        expect(self.page).to_have_url(re.compile(rf".*/{url_paths.SUPPLIER_DETAILS}"))
         expect(self.page.get_by_label("Postcode")).to_be_visible()
         expect(self.page.get_by_label("County (optional)")).to_be_visible()
 
     def test_non_uk_options_returns_non_uk_address_capture(self):
-        self.page.goto(self.base_url)
-        self.page.get_by_role("link", name="Reset session").click()
         self.page.get_by_role("link", name="Your details").click()
         self.create_reporter_details(self.page, "I'm an owner")
         self.page.get_by_role("link", name="2. About the person or").click()
@@ -61,14 +55,12 @@ class TestWhereWereTheGoodsSuppliedFrom(conftest.PlaywrightTestBase):
         self.page.get_by_role("heading", name="Where were the goods,").click()
         self.page.get_by_label("Outside the UK").check()
         self.page.get_by_role("button", name="Continue").click()
-        expect(self.page).to_have_url(re.compile(r".*/supplier-details"))
+        expect(self.page).to_have_url(re.compile(rf".*/{url_paths.SUPPLIER_DETAILS}"))
         expect(self.page.get_by_label("Address line 3 (optional)")).to_be_visible()
         expect(self.page.get_by_label("Address line 4 (optional)")).to_be_visible()
         expect(self.page.get_by_label("Country")).to_be_visible()
 
     def test_i_do_not_know_returns_supplied_to(self):
-        self.page.goto(self.base_url)
-        self.page.get_by_role("link", name="Reset session").click()
         self.page.get_by_role("link", name="Your details").click()
         self.create_reporter_details(self.page, "I'm an owner")
         self.page.get_by_role("link", name="2. About the person or").click()
@@ -79,11 +71,9 @@ class TestWhereWereTheGoodsSuppliedFrom(conftest.PlaywrightTestBase):
         self.page.get_by_role("heading", name="Where were the goods,").click()
         self.page.get_by_label("I do not know").check()
         self.page.get_by_role("button", name="Continue").click()
-        expect(self.page).to_have_url(re.compile(r".*/location-of-end-user"))
+        expect(self.page).to_have_url(re.compile(rf".*/{url_paths.LOCATION_END_USER}"))
 
     def test_not_supplied_yet_returns_made_available_from(self):
-        self.page.goto(self.base_url)
-        self.page.get_by_role("link", name="Reset session").click()
         self.page.get_by_role("link", name="Your details").click()
         self.create_reporter_details(self.page, "I'm an owner")
         self.page.get_by_role("link", name="2. About the person or").click()
@@ -94,11 +84,9 @@ class TestWhereWereTheGoodsSuppliedFrom(conftest.PlaywrightTestBase):
         self.page.get_by_role("heading", name="Where were the goods,").click()
         self.page.get_by_label("They have not been supplied yet").check()
         self.page.get_by_role("button", name="Continue").click()
-        expect(self.page).to_have_url(re.compile(r".*/location-where-made-available-from"))
+        expect(self.page).to_have_url(re.compile(rf".*/{url_paths.LOCATION_MADE_AVAILABLE_FROM}"))
 
     def test_breacher_is_supplier_returns_supplied_to(self):
-        self.page.goto(self.base_url)
-        self.page.get_by_role("link", name="Reset session").click()
         self.page.get_by_role("link", name="Your details").click()
         self.create_reporter_details(self.page, "I'm an owner")
         self.page.get_by_role("link", name="2. About the person or").click()
@@ -109,4 +97,4 @@ class TestWhereWereTheGoodsSuppliedFrom(conftest.PlaywrightTestBase):
         self.page.get_by_role("heading", name="Where were the goods,").click()
         self.page.get_by_label("Germany Lane, Germany Avenue, Germany Town, Germany").check()
         self.page.get_by_role("button", name="Continue").click()
-        expect(self.page).to_have_url(re.compile(r".*/location-of-end-user"))
+        expect(self.page).to_have_url(re.compile(rf".*/{url_paths.LOCATION_END_USER}"))
--- tests/test_frontend/test_report_a_suspected_breach/test_your_details/test_email.py
+++ tests/test_frontend/test_report_a_suspected_breach/test_your_details/test_email.py
@@ -2,7 +2,7 @@ import re
 
 from playwright.sync_api import expect
 
-from tests.test_frontend import conftest
+from tests.test_frontend import conftest, url_paths
 
 
 class TestEmail(conftest.PlaywrightTestBase):
@@ -11,19 +11,15 @@ class TestEmail(conftest.PlaywrightTestBase):
     """
 
     def test_correct_email_goes_to_verify_page(self):
-        self.page.goto(self.base_url)
-        self.page.get_by_role("link", name="Reset session").click()
         self.page.get_by_role("link", name="Your details").click()
         self.reporter_professional_relationship(self.page, "owner")
         self.page.get_by_role("button", name="Continue").click()
         self.page.get_by_label("What is your email address?").click()
         self.page.get_by_label("What is your email address?").fill("test@gmail.com")
         self.page.get_by_role("button", name="Continue").click()
-        expect(self.page).to_have_url(re.compile(r".*/enter-security-code"))
+        expect(self.page).to_have_url(re.compile(rf".*/{url_paths.SECURITY_CODE}"))
 
     def test_no_email_raises_error(self):
-        self.page.goto(self.base_url)
-        self.page.get_by_role("link", name="Reset session").click()
         self.page.get_by_role("link", name="Your details").click()
         self.reporter_professional_relationship(self.page, "owner")
         self.page.get_by_role("button", name="Continue").click()
@@ -31,11 +27,9 @@ class TestEmail(conftest.PlaywrightTestBase):
         self.page.get_by_role("button", name="Continue").click()
         expect(self.page.get_by_label("There is a problem")).to_be_visible()
         expect(self.page.get_by_role("link", name="Enter your email address")).to_be_visible()
-        expect(self.page).to_have_url(re.compile(r".*/your-email-address"))
+        expect(self.page).to_have_url(re.compile(rf".*/{url_paths.EMAIL_ADDRESS}"))
 
     def test_incorrect_format_raises_error(self):
-        self.page.goto(self.base_url)
-        self.page.get_by_role("link", name="Reset session").click()
         self.page.get_by_role("link", name="Your details").click()
         self.reporter_professional_relationship(self.page, "owner")
         self.page.get_by_role("button", name="Continue").click()
@@ -44,4 +38,4 @@ class TestEmail(conftest.PlaywrightTestBase):
         self.page.get_by_role("button", name="Continue").click()
         expect(self.page.get_by_label("There is a problem")).to_be_visible()
         expect(self.page.get_by_role("link", name="Enter an email in the correct format")).to_be_visible()
-        expect(self.page).to_have_url(re.compile(r".*/your-email-address"))
+        expect(self.page).to_have_url(re.compile(rf".*/{url_paths.EMAIL_ADDRESS}"))
