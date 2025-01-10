2e14b4a5c965d3867b2b5e22c7aa1e341cba2092
Abdallah Harun <abdallah.harun@digital.trade.gov.uk>
2025-01-10 11:26:12 +0000

    DST-861 - Adding content changes (#283)

--- django_app/feedback/urls.py
+++ django_app/feedback/urls.py
@@ -6,5 +6,5 @@ app_name = "feedback"
 
 urlpatterns = [
     path("", views.ProvideFullFeedbackView.as_view(), name="collect_full_feedback"),
-    path("done", views.FeedbackDoneView.as_view(), name="feedback_done"),
+    path("feedback-sent", views.FeedbackDoneView.as_view(), name="feedback_done"),
 ]
