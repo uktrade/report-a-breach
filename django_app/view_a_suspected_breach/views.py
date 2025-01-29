1278f3bdb7de05cba0af067ff31e19d9ea1b946c
jenriordan <jennifer.riordan@digital.trade.gov.uk>
2025-01-29 17:10:34 +0000

    wip starting viewer

--- django_app/view_a_suspected_breach/views.py
+++ django_app/view_a_suspected_breach/views.py
@@ -7,13 +7,17 @@ from django.contrib.auth.models import User
 from django.db.models import QuerySet
 from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, reverse
+from django.template.loader import render_to_string
 from django.urls import reverse_lazy
 from django.utils.decorators import method_decorator
+from django.utils.safestring import mark_safe
 from django.views.generic import DetailView, ListView, RedirectView, TemplateView
 from feedback.models import FeedbackItem
+from playwright.sync_api import sync_playwright
 from report_a_suspected_breach.models import Breach
 from utils.breach_report import get_breach_context_data
 
+from ..utils import breach_report
 from .mixins import ActiveUserRequiredMixin, StaffUserOnlyMixin
 
 logger = logging.getLogger(__name__)
@@ -116,3 +120,38 @@ class ViewFeedbackView(LoginRequiredMixin, ActiveUserRequiredMixin, DetailView):
     model = FeedbackItem
     template_name = "view_a_suspected_breach/view_feedback.html"
     context_object_name = "feedback"
+
+
+@method_decorator(require_view_a_breach(), name="dispatch")
+class DownloadPDFView(LoginRequiredMixin, ActiveUserRequiredMixin, DetailView):
+    template_name = "view_a_suspected_breach/report_pdf.html"
+
+    def get_object(self, queryset=None) -> Breach:
+        self.breach = get_object_or_404(Breach, reference=self.request.GET.get("reference", ""))
+        return self.breach
+
+    def get(self, request: HttpRequest, *args: object, **kwargs: object) -> HttpResponse:
+        context_data = self.get_context_data(**kwargs)
+        filename = f"report-{context_data['reference']}.pdf"
+        pdf_data = None
+        template_string = render_to_string(self.template_name, context=context_data)
+
+        with sync_playwright() as playwright:
+            browser = playwright.chromium.launch(headless=True)
+            page = browser.new_page()
+            page.set_content(mark_safe(template_string))
+            page.wait_for_function("document.fonts.ready.then(fonts => fonts.status === 'loaded')")
+            pdf_data = page.pdf(format="A4")
+            browser.close()
+
+        response = HttpResponse(pdf_data, content_type="application/pdf")
+        response["Content-Disposition"] = f"inline; filename={filename}"
+
+        return response
+
+    def get_context_data(self, *args: object, **kwargs: object) -> dict[str, Any]:
+        self.object = get_object_or_404(Breach, reference=self.request.GET.get("reference", ""))
+        context = super().get_context_data(**kwargs)
+        breach_context_data = breach_report.get_breach_context_data(self.object)
+        context.update(breach_context_data)
+        return context
