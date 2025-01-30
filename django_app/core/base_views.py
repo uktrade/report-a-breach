fd58fbb0387cfe7a4bb907492af538f82cad64fd
jenriordan <jennifer.riordan@digital.trade.gov.uk>
2025-01-30 15:46:05 +0000

    object debug

--- django_app/core/base_views.py
+++ django_app/core/base_views.py
@@ -128,6 +128,7 @@ class BaseDownloadPDFView(DetailView):
         return response
 
     def get_context_data(self, **kwargs: object) -> dict[str, Any]:
+        self.object = []
         context = super().get_context_data(**kwargs)
         context["header"] = self.header
         context["reference"] = self.reference
