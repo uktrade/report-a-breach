fa81ab4dfdee71dc79516981c79d219feef298b9
Christopher Pettinga <chris.pettinga@digital.trade.gov.uk>
2025-02-10 21:06:12 +0000

    fixing tests

--- tests/test_unit/test_report_a_suspected_breach/test_utils.py
+++ tests/test_unit/test_report_a_suspected_breach/test_utils.py
@@ -1,4 +1,5 @@
 import builtins
+from builtins import __import__ as builtin_import
 
 from report_a_suspected_breach.utils import get_active_regimes
 
@@ -12,7 +13,11 @@ def test_get_active_regimes_normal():
 
 def test_get_active_regimes_import_error(monkeypatch):
     def mock_import(*args, **kwargs):
-        raise ImportError
+        if args[3][0] == "active_regimes":
+            raise ImportError
+        else:
+            # we need to provide some route to the original import as the teardown of the test will fail otherwise
+            return builtin_import(*args, **kwargs)
 
     monkeypatch.setattr(builtins, "__import__", mock_import)
     regimes = get_active_regimes()
