62aab427412626c947d7e26567ba7f4f275d1887
cencorroll <abdallah.harun@digital.trade.gov.uk>
2025-04-14 15:11:16 +0100

    DST-1034 - Adding 'Error: ' to title browser tab text when errors appear

--- tests/test_frontend/test_core/test_base_form_step_title_error_message.py
+++ tests/test_frontend/test_core/test_base_form_step_title_error_message.py
@@ -7,9 +7,8 @@ from tests.test_frontend.conftest import PlaywrightTestBase
 
 class TestBaseFormStepTitleErrorMessage(PlaywrightTestBase):
     def test_error_in_title(self):
-        self.page.goto("http://report-a-suspected-breach:8001/report/task-list")
         self.page.get_by_role("link", name="Your details").click()
         # Clicking through without inputting an email address
         self.page.get_by_role("button", name="Continue").click()
 
-        expect(self.page).to_have_title(re.compile("Error: ", re.IGNORECASE))
+        expect(self.page).to_have_title(re.compile("Error: "))
