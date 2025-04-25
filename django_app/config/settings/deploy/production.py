0d77d84f954fcc6d60f209a7ba3e56ce11feee37
Morgan Rees <65167892+morganmaerees@users.noreply.github.com>
2025-04-25 14:59:20 +0100

    DST-1056: create new production sites (#341)
    Co-authored-by: Morgan Rees <morganrees@DBT000491.local>

--- django_app/config/settings/deploy/production.py
+++ django_app/config/settings/deploy/production.py
@@ -8,3 +8,5 @@ ENVIRONMENT = "production"
 # Django extra production sites
 REPORT_A_SUSPECTED_BREACH_EXTRA_DOMAIN = env.report_a_suspected_breach_extra_domain
 VIEW_A_SUSPECTED_BREACH_EXTRA_DOMAIN = env.view_a_suspected_breach_extra_domain
+REPORT_A_SUSPECTED_BREACH_SERVICE_DOMAIN = env.report_a_suspected_breach_service_domain
+VIEW_A_SUSPECTED_BREACH_SERVICE_DOMAIN = env.view_a_suspected_breach_service_domain
